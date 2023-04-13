837b79734b11b79d71992f62b6e630b4292a91c0
AkmalRendiansyah <127172771+AkmalRendiansyah@users.noreply.github.com>
2023-04-13 12:24:38 +0700

    Add files via upload

---
+++ Pbo_2211083040/src/akmal_130423/Human.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akmal_130423;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class Human extends LivingThing
+{
+public void walk(){
+    System.out.println("Human walks...");
+}
+}
